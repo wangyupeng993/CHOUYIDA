015045db14917ea3eed641125e6bb52ecdee9d88
universe-peng <3464842637@qq.com>
2020-06-18 00:41:37 +0800

    团队介绍

--- src/views/pages/team/index.vue
+++ src/views/pages/team/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <scroll-view @handle-scroll="mousewheel" ref="scrollBar" :scroll-y="!isPC">
+    <scroll-view @handle-scroll="mousewheel" ref="scrollBar" :scroll-y="!isPC" v-loading="Loading">
         <div>
             <img width="100%" :style="`min-height:${isPC?'':(590/46.875)}rem;`"
                  :class="`${isPC?'':'object-fit-cover'}`"
@@ -12,24 +12,23 @@
 
             <div class="padding-tb-df">
                 <ul class="text-center">
-                    <li v-for="item in switchType" :key="item.name" :class="[
-                    'inline-block text-grey',
-                    'padding-tb-xs pointer',
-                    `${item.className}`
-                    ]"><span :class="`inline-block ${isPC?'padding-lr-sm':'padding-lr-xs'}`">
-                        {{item.name}}</span> <i v-if="item.isSlash">/</i>
+                    <li v-for="item in Nav" :key="item.id" :class="[
+                        'inline-block padding-tb-xs pointer',`${item.className} text-grey`
+                        ]" @click="switchTeam(item.id)"><span :class="[
+                        'inline-block padding-lr-df',
+                        `${navActive == item.id?'text-green':'text-grey'}`
+                        ]">{{item.name}}</span><i v-if="item.isSlash">/</i>
                     </li>
                 </ul>
             </div>
 
-            <div class="flex padding-bottom-df">
+            <div class="flex padding-bottom-df padding-lr-xs flex-wrap-wrap">
                 <div :class="[
-                `team-info shadow ${isPC?'basis-xs':'basis-df'}`,
-                'bg-white padding-sm radius-lg pointer',
-                'flex items-center justify-center relative hidden',
-                `${(index%2) === 1&&isPC?'margin-lr':'margin-lr-sm'}`
-                ]" v-for="(item,index) in designer" :key="index">
-                    <div class="text-center">
+                `team-info ${isPC?'basis-xm padding-sm margin-lr-sm shadow':'basis-df'}`,
+                'bg-white radius-lg pointer margin-bottom-df',
+                'relative hidden',
+                ]" v-for="(item,index) in team" :key="index">
+                    <div :class="`text-center ${isPC?'':'margin-xs padding-sm shadow radius-lg'}`">
                         <div class="padding-xs round inline-block"
                              :style="`width:${item.imgWidth};height:${item.imgWidth};`">
                             <img class="app-main" src="@/assets/images/home/userImg.png" alt="" />
@@ -38,19 +37,19 @@
                             {{item.name}}
                         </p>
                         <p :class="`margin-top-xs text-black text-center ${isPC?'text-sm':'text-df'}`">
-                            {{item.skill}}
+                            {{item.type_name}}
                         </p>
                         <div v-if="isPC" :class="[
                         'team-description',
                         'text-justify line-height-xs bg-gradualDarkgreen radius-lg hidden',
                         'app-main absolute absolute-t absolute-l text-white text-sm'
-                        ]"><p class="margin-sm">{{item.content}}</p>
+                        ]"><p class="margin-sm">{{item.introduction}}</p>
                         </div>
 
                         <div v-if="!isPC" :class="[
                         'text-justify line-height-xs',
                         'padding-tb-sm text-black text-sm'
-                        ]"><p>{{item.content}}</p>
+                        ]"><p>{{item.introduction}}</p>
                         </div>
                     </div>
                 </div>
@@ -63,55 +62,66 @@
     </scroll-view>
 </template>
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import ObjectDetection from "@/api/methods/validator";
-    import Footer from "@/components/Footer/index.vue";
-    import Pagination from "@/components/pagination/index.vue";
-    @Component({
-        components:{Footer,Pagination}
-    })
-    export default class Team extends Vue {
-        isPC = ObjectDetection.isPCBroswer();
-        switchType = [{
-            name:'创始',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: true
-        },{
-            name:'创意团队',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: true
-        },{
-            name:'项目经理',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: true
-        },{
-            name:'风险控制',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: true
-        },{
-            name:'网络技术',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: true
-        },{
-            name:'顾问团队',
-            className: `${this.isPC?'text-sm':'text-df'}`,
-            isSlash: false
-        }];
-        designer = [{
-            name: '王利军',
-            skill: '创始人   CEO',
-            content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容......',
-            imgWidth: `${this.isPC?'130px':(130/46.875)+'rem'}`,
-            isPC: this.isPC
-        },{
-            name: '王利军',
-            skill: '创始人   CEO',
-            content: '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容......',
-            imgWidth: `${this.isPC?'130px':(130/46.875)+'rem'}`,
-            isPC: this.isPC
-        }];
-        mousewheel = (ev: Element) => {
-            this.$store.commit('getScrollTop',ev.scrollTop);
+import { Component, Vue, Watch} from 'vue-property-decorator';
+import ObjectDetection from "@/api/methods/validator";
+import Footer from "@/components/Footer/index.vue";
+import Pagination from "@/components/pagination/index.vue";
+import {Getter} from "vuex-class";
+import service from "@/api/request";
+@Component({
+    components:{Footer,Pagination}
+})
+export default class Team extends Vue {
+    private isPC: boolean;
+    private team: object[];
+    private navActive: number;
+    private Loading: boolean;
+    private paging: ServicePagination;
+    constructor () {
+        super();
+        this.isPC = ObjectDetection.isPCBroswer();
+        this.team = [];
+        this.navActive = 0;
+        this.Loading = false;
+        this.paging = {type: 0,limit: 8,page: 1}
+    }
+    @Getter('teamNav') Nav: any
+    @Watch('Nav')
+    NavChange (nav: [{id: number}]) {
+        this.paging = {...this.paging,type: nav[0].id}
+        this.getTeamList(this.paging);
+    }
+
+    async getTeamList (params: ServicePagination) {
+        this.navActive = params.type;
+        this.Loading = true;
+        service.getTeamList(params).then(response => {
+            this.Loading = false;
+            this.team = response.data.list.map(item => {
+                return {
+                    ...item,
+                    imgWidth: `${this.isPC?'130px':(130/46.875)+'rem'}`,
+                    isPC: this.isPC
+                }
+            });
+        }).catch(() => {this.Loading = false;});
+    }
+
+    mousewheel = (ev: Element) => {
+        this.$store.commit('getScrollTop',ev.scrollTop);
+    }
+
+    switchTeam (id: number) {
+        if (id === this.navActive) return ;
+        this.paging = {limit: 6,page: 1,type: id};
+        this.getTeamList(this.paging);
+    }
+
+    mounted(): void {
+        if (this.Nav[0]) {
+            this.paging = {...this.paging,type: this.Nav[0].id}
+            this.getTeamList(this.paging);
         }
     }
+}
 </script>
