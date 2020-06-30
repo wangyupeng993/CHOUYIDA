ee17f1ae57d3193d6ec52fe48b5311e7e4f97013
universe-peng <3464842637@qq.com>
2020-07-01 00:11:33 +0800

    更换域名

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const queryString = require('querystring');
-const URL = process.env.NODE_ENV === 'development' ? '/' : 'https://www.chouyida.com';
+const URL = process.env.NODE_ENV === 'development' ? '/' : 'https://api.chouyida.com';
 
 const service = axios.create({
     baseURL: URL,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.NODE_ENV === 'development'?'https://www.chouyida.com':'/';
+const baseURL = process.env.NODE_ENV === 'development'?'https://api.chouyida.com':'/';
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
